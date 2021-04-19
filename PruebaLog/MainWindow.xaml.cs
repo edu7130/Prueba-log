2708b4d53d7d0e1aee6a6eb4867fea87f27ad178
Eduardo <Eduardo@EDUGAMES>
2021-04-19 20:14:06 -0300

    first commit

---
+++ PruebaLog/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PruebaLog
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); 
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Prueba"); 
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            log.Error("Dice Error1123");
+        }
+    }
+}
